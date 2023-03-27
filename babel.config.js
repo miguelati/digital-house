e974016458031932be5c97930c99258286211e72
Miguel Godoy <miguelgodoyg@gmail.com>
2023-03-27 10:11:48 -0400

    chore: Add alias path & others configs

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,25 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    '@babel/plugin-proposal-export-namespace-from',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '^~/(.+)': './src/\\1',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.json',
+          '.tsx',
+          '.ts',
+          '.native.js',
+        ],
+      },
+    ],
+  ],
 };
