4a3e2478b4f659f6ac3d79f1d2e56fa0c380cb44
Miguel Godoy <miguelgodoyg@gmail.com>
2023-03-28 20:25:29 -0400

    fix: Add Query provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,26 @@ import Config from 'react-native-config';
 import {NavigationContainer} from '@react-navigation/native';
 import {configureClient} from '~/api/client';
 import {AppStack} from '~/layouts';
+import {QueryClient, QueryClientProvider} from 'react-query';
 
 configureClient({
   baseURL: Config.API_URL,
 });
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      cacheTime: 1000 * 60 * 60 * 48,
+    },
+  },
+});
+
 const App = (): JSX.Element => {
   return (
     <NavigationContainer>
-      <AppStack />
+      <QueryClientProvider client={queryClient} contextSharing>
+        <AppStack />
+      </QueryClientProvider>
     </NavigationContainer>
   );
 };
